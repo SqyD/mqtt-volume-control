8bb095460d8833420ff011e2535e206103498f43
Paul Krischer <paul@krischer.nl>
2019-10-03 21:46:32 +0100

    Still laying out foundations. Don't even try to look at this yet let alone try to use it.

--- mqtt-volume-control.py
+++ mqtt-volume-control.py
@@ -10,32 +10,28 @@ default_volume: 0.8
 broker = '192.168.178.5'
 user = 'username'
 password = 'yoursecret'
+topic_prefix = 'homeassistant'
 
 # i2c configuration
-
-
-topic_prefix = 'living/'
-# vc_state =  'tv/volume'
-# vc_set_topic 'tv/volume/set
-# vc_up_topic: 'tv/volume/up
-# vc_down_topic: 'tv/volume/down'
-# vc_muted_state: 'tv/volume/muted'
-# vc_mute_set: 'tv/volume/mute/set
 
 class VolumeControl:
 
   def __init__(self, id):
+    self.id = id
     self.volume = default_volume
     self.muted = 'OFF'
+    self.volume_topic = topic_prefix + '/sensor/' + self.id + '/volume'
+    self.mute_topic = topic_prefix + '/binary_sensor/' + self.id + '/mute'
     self.publish()
 
+  def discovery(self):
+  
   def publish(self):
     # publish current volume level
-    mqttc.publish(topic_prefix + self.id + '/volume', self.volume)
-    mqttc.publish(topic_prefix + self.id + '/muted', self.muted)
+    mqttc.publish(self.volume_topic, self.volume)
+    mqttc.publish(self.mute_topic, self.muted)
 
 class PT2259VolumeControl(VolumeControl):
-
 
 def on_message(client, userdata, message):
   payload = str(message.payload.decode("utf-8"))
@@ -45,9 +41,8 @@ def on_message(client, userdata, message):
 
 def vc_set(volume):
 
-
-
-
+# import smbus
+# bus = smbus.SMBus(1)
 # bus.write_byte(0x44,0xF0)
 # >>> bus.write_i2c_block_data(0x44,0x74,[0xE2,0xD0])
 # >>> bus.write_i2c_block_data(0x44,0x74,[0xE0,0xD0])
